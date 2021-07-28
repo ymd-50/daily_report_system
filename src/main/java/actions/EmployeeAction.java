a1b6b7d1704d8d2407221bd645febb3398e8ea96
ymd20 <ymd20@DESKTOP-FA5I8Q6>
2021-07-28 12:21:12 +0900

    従業員のshowアクション追加

--- src/main/java/actions/EmployeeAction.java
+++ src/main/java/actions/EmployeeAction.java
@@ -88,8 +88,19 @@ public class EmployeeAction extends ActionBase {
 
    }
 
-   public void show() throws ServletException, IOException{
 
+
+   public void show() throws ServletException, IOException{
+       EmployeeView ev = service.findOne(toNumber(getRequestParam(AttributeConst.EMP_ID)));
+
+       if(ev == null || ev.getDeleteFlag() == AttributeConst.DEL_FLAG_TRUE.getIntegerValue()) {
+
+           forward(ForwardConst.FW_ERR_UNKNOWN);
+           return;
+       }
+
+       putRequestScope(AttributeConst.EMPLOYEE, ev);
+       forward(ForwardConst.FW_EMP_SHOW);
    }
 
 }
